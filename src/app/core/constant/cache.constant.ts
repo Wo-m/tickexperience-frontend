02af19ef9fe88342598bb3b0b2b42f981e6ad975
Wo-m <dom.thompson458@gmail.com>
2023-08-14 15:58:26 +1000

    cache using sessionStorage

---
+++ src/app/core/constant/cache.constant.ts
@@ -0,0 +1,6 @@
+/**
+ * Store keys used in SessionStorage.
+ */
+export class CacheConstants {
+  public static token: string = "token";
+}
--- src/app/core/service/auth.service.ts
+++ src/app/core/service/auth.service.ts
@@ -1,7 +1,8 @@
-import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Token } from '../model/token.model';
 import { Login } from '../model/login.model';
+import { CacheConstants } from '../constant/cache.constant';
 
 /**
  * Singleton Service for handling authentication
@@ -12,7 +13,6 @@ import { Login } from '../model/login.model';
 })
 export class AuthService {
 
-  private static token: string = "";
   private base_url: string = "http://127.0.0.1:8080/api/auth/";
 
   constructor(private http: HttpClient) {
@@ -23,20 +23,23 @@ export class AuthService {
 
     this.http.post(this.base_url + "login", body)
       .subscribe((data: Token) =>
-        AuthService.token = data.token!);
+        // store token in browser session storage
+        sessionStorage.setItem(CacheConstants.token, data.token!));
   }
 
-  // Static Methods to avoid injection
-  static logout() {
-    this.token = '';
+  logout() {
+    let body = {"token": AuthService.getToken()};
+
+    sessionStorage.removeItem(CacheConstants.token);
+    this.http.post(this.base_url + "logout", body).subscribe();
   }
 
   static getToken(): string {
-    return this.token;
+    return sessionStorage.getItem(CacheConstants.token)!;
   }
 
   static isAuthenticated(): boolean {
-    return this.token != "";
+    return sessionStorage.getItem(CacheConstants.token) != '';
   }
 
 }
