d344a940c2c0ccdb09489bb0894cb67670d1b5fe
hcmgr <harryrackemann@gmail.com>
2023-08-30 15:46:34 +1000

    bullshit changes

--- src/app/core/service/user.service.ts
+++ src/app/core/service/user.service.ts
@@ -7,7 +7,6 @@ import { AuthUtils } from '../Utils/auth.utils';
 })
 export class UserService {
 
-
   private base_url: string = "http://127.0.0.1:8080/api/authed/user";
   constructor(private http: HttpClient) { }
 
