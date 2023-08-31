3616ea706dfa4ea1335b0f8f7393bcba34ab89b1
Wo-m <dom.thompson458@gmail.com>
2023-08-31 15:42:21 +1000

    inject sports service

--- src/app/core/service/sport.service.ts
+++ src/app/core/service/sport.service.ts
@@ -1,9 +1,12 @@
+import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+@Injectable({
+  providedIn: 'root'
+})
 export class SportService {
 
   private base_url: string = "http://127.0.0.1:8080/api/sport";
-
   constructor(private http: HttpClient) { }
 
   getAllSports() {
