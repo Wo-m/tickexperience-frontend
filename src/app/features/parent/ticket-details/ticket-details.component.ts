46c02dc51c1021ed8c75533add56f22c395c1743
hrmcnee <hrmcnee502@gmail.com>
2023-09-13 16:58:51 +1000

    More page set up - no formatting, just general layout of elements

--- src/app/features/parent/ticket-details/ticket-details.component.ts
+++ src/app/features/parent/ticket-details/ticket-details.component.ts
@@ -16,9 +16,9 @@ export class TicketDetailsComponent implements OnInit {
   ticket = {userId: 1,
                                         ticketId: 2,
                                         seatNumber: 101,
-                                        venue: "Gabba",
-                                        event: "Cricket",
-                                        date: new Date("2032-05-27T11:30")}
+                                        venue: "Chandler",
+                                        event: "Swimming",
+                                        date: new Date("2032-11-19T11:30")}
   countdownTime: number[] = [0,0, 0]; //[years, months, days]
 
   constructor(private route: ActivatedRoute,
@@ -41,9 +41,12 @@ export class TicketDetailsComponent implements OnInit {
       let distance = this.ticket.date.getTime() - now;
 
       // Time calculations for days, hours, minutes and seconds
-      let days = Math.floor(distance / (1000 * 60 * 60 * 24));
-      let months = Math.floor(distance/(1000 * 60 * 60 * 24 * 7));
-      let years = Math.floor(distance/(1000 * 60 * 60 * 24 * 7));
+      let years = Math.floor(distance/(1000 * 60 * 60 * 24 * 365));
+      let yearsDif = distance - (years * 1000 * 60 * 60 * 24 * 365);
+      let months = Math.floor(12 * (yearsDif /(1000 * 60 * 60 * 24 * 365)));
+      let monthsDif = yearsDif - (months / 12 * (1000 * 60 * 60 * 24 * 365));
+      let days = Math.floor(monthsDif / (1000 * 60 * 60 * 24));
+
 
       this.countdownTime = [years, months, days];
   }
