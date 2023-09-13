fb663691d597b11cd769e5e2e02582cc85f6cdc5
hcmgr <harryrackemann@gmail.com>
2023-09-13 12:11:26 +1000

    added MyAccountDetails model which component ingests and displays

--- src/app/features/parent/my-account/my-account.component.ts
+++ src/app/features/parent/my-account/my-account.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {UserService} from "../../../core/service/user.service";
+import {MyAccountDetails} from "../../../core/model/my-account.model";
 
 @Component({
   selector: 'app-my-account',
@@ -7,4 +9,20 @@ import { Component } from '@angular/core';
 })
 export class MyAccountComponent {
 
+  myAccountDetails: MyAccountDetails = {
+    name: 'John Doe',
+    username: 'johndoe123',
+    password: '********', // You can replace this with a real hashed password if needed
+    email: 'johndoe@example.com',
+    mobile: '+61462535294',
+  };
+
+  constructor(private userService: UserService) {}
+
+  ngOnInit() {
+    // Fetch user data when the component initializes
+    this.userService.getMyAccountDetails().subscribe((userData) => {
+      this.myAccountDetails = userData;
+    });
+  }
 }
