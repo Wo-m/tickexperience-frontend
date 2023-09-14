540b7be5bbaf0ece581cd30245a477c1367f80fd
Wo-m <dom.thompson458@gmail.com>
2023-09-14 17:35:27 +1000

    event service

---
+++ src/app/core/service/event.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Event } from '../model/event.model';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class EventService {
+
+    private base_url: string = 'http://127.0.0.1:8080/api/event';
+
+    constructor(private http: HttpClient) {
+    }
+
+    getEvent(eventId: number) {
+        return this.http.get<Event>(this.base_url + `/${eventId}`);
+    }
+}
