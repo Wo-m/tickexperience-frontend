afe63ac77e54ea3f2735692185fea5f4d005b33c
hcmgr <harryrackemann@gmail.com>
2023-10-17 17:40:47 +1000

    changed default backend URL back to localhost

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -7,8 +7,8 @@ export const environment = {
   googleMapsApiKey: "AIzaSyC3G27zHDnlWeY3jVhfqOdFlBG8o1TzZd0",
 
   // urls //
-  backendBaseUrl: "http://170.64.185.134:8080/api", // NOTE: ONLY USE THIS IN PRODUCTION (or for testing)
-  // backendBaseUrl: "http://127.0.0.1:8080/api",
+  // backendBaseUrl: "http://170.64.185.134:8080/api", // NOTE: ONLY USE THIS IN PRODUCTION (or for testing)
+  backendBaseUrl: "http://127.0.0.1:8080/api",
 
   googleMapsBaseUrl: "https://www.google.com/maps/embed/v1/place"
 };
